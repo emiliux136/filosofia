59f0a671f2ca0872896ea60232520032d8942f74
emiliux136 <emiliojaviergarciaburgos@gmail.com>
2025-07-03 19:18:05 +0200

    cambios que hice en la biblioteca

---
+++ filo/hilos.c
@@ -0,0 +1,35 @@
+#include "philo.h"
+
+// # define W_THR "Thread creation error\n
+
+int thread_maker(t_program *program, pthread_mutex_t *forks)
+{
+    pthread_t   observer;
+    int         i;
+    if (pthread_create(&observer, NULL, &monitor, program->philos) != 0)
+        destroyer(W_THR, program, forks)
+    /* a la funcion pthread_create, tengo que pasarle otra función 
+    mas bien su direccion de memoria para que a la hora de crear 
+    el hilo la añada. Por eso voy a hacer primero la rutina */
+}
+
+void    destroyer(char *str, t_program *program, pthread_mutex_t *forks)
+{
+    int i;
+
+    i = 0;
+    ft_error(str, 1, 2);
+    pthread_mutex_destroy(&program->write_lock);
+    pthread_mutex_destroy(&program->meal_lock);
+    pthread_mutex_destroy(&program->dead_lock);
+    while (i < program->philos[0].num_philos)
+    {
+        pthread_mutex_destroy(&forks[i]);
+        i++;
+    }
+}
+
+void    ft_print(char *str, t_philo *philo, int id)
+{
+    
+}
---
+++ filo/rutina.c
@@ -0,0 +1,6 @@
+#include "philo.h"
+
+void    eat(t_philo *philo)
+{
+    pthread_mutex_lock(philo->r_fork);
+}
